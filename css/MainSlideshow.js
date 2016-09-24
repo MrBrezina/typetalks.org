d3e4a2c886a6ae67bdabc1660782eeea4a3ac001
David Březina <brezina@davi.cz>
2016-09-24 19:02:28 +0100

    Renamed folders _ss and _images

--- css/MainSlideshow.js
+++ css/MainSlideshow.js
@@ -2,10 +2,10 @@ var mygallery=new fadeSlideShow({
 	wrapperid: "slideshow", //ID of blank DIV on page to house Slideshow
 	dimensions: [450, 200], //width/height of gallery in pixels. Should reflect dimensions of largest image
 	imagearray: [
-		["_images/bw_favu-type-workshop-1.jpg", "", "", ""],
-		["_images/bw_favu-type-workshop-2.jpg", "", "", ""],
-		["_images/bw_favu-type-workshop-3.jpg", "", "", ""],
-		["_images/bw_favu-type-workshop-4.jpg", "", "", ""] //<--no trailing comma after very last image element!
+		["img/bw_favu-type-workshop-1.jpg", "", "", ""],
+		["img/bw_favu-type-workshop-2.jpg", "", "", ""],
+		["img/bw_favu-type-workshop-3.jpg", "", "", ""],
+		["img/bw_favu-type-workshop-4.jpg", "", "", ""] //<--no trailing comma after very last image element!
 	],
 	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:true},
 	persist: true, //remember last viewed slide and recall within same session?
