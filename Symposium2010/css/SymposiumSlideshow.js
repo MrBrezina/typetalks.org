50c6062f5836693b50298163f5a6112cdd830fbe
David Březina <brezina@davi.cz>
2016-09-24 19:05:09 +0100

    And in slideshows

--- Symposium2010/css/SymposiumSlideshow.js
+++ Symposium2010/css/SymposiumSlideshow.js
@@ -2,15 +2,15 @@ var mygallery=new fadeSlideShow({
 	wrapperid: "slideshow", //ID of blank DIV on page to house Slideshow
 	dimensions: [450, 200], //width/height of gallery in pixels. Should reflect dimensions of largest image
 	imagearray: [
-		["_images/bw_brousil.jpg", "", "", ""],
-		["_images/bw_burian.jpg", "", "", ""],
-		["_images/bw_hardwig.jpg", "", "", ""],
-		["_images/bw_hardwig-1.jpg", "", "", ""],
-		["_images/bw_hardwig-2.jpg", "", "", ""],
-		["_images/bw_hardwig-3.jpg", "", "", ""],
-		["_images/bw_hochleitner.jpg", "", "", ""],
-		["_images/bw_keller.jpg", "", "", ""],
-		["_images/bw_reynolds.jpg", "", "", ""] //<--no trailing comma after very last image element!
+		["img/bw_brousil.jpg", "", "", ""],
+		["img/bw_burian.jpg", "", "", ""],
+		["img/bw_hardwig.jpg", "", "", ""],
+		["img/bw_hardwig-1.jpg", "", "", ""],
+		["img/bw_hardwig-2.jpg", "", "", ""],
+		["img/bw_hardwig-3.jpg", "", "", ""],
+		["img/bw_hochleitner.jpg", "", "", ""],
+		["img/bw_keller.jpg", "", "", ""],
+		["img/bw_reynolds.jpg", "", "", ""] //<--no trailing comma after very last image element!
 	],
 	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:true},
 	persist: true, //remember last viewed slide and recall within same session?
--- Symposium2011/css/SymposiumSlideshow.js
+++ Symposium2011/css/SymposiumSlideshow.js
@@ -2,12 +2,12 @@ var mygallery=new fadeSlideShow({
 	wrapperid: "slideshow", //ID of blank DIV on page to house Slideshow
 	dimensions: [735, 320], //width/height of gallery in pixels. Should reflect dimensions of largest image
 	imagearray: [
-		["_images/bw_adam_03.jpg", "", "", ""],
-		["_images/bw_indra_01.jpg", "", "", ""],
-		["_images/bw_akiem_01.jpg", "", "", ""],
-		["_images/bw_jacek_01.jpg", "", "", ""],
-		["_images/bw_jasso_01.jpg", "", "", ""],
-		["_images/bw_martin_01.jpg", "", "", ""] //<--no trailing comma after very last image element!
+		["img/bw_adam_03.jpg", "", "", ""],
+		["img/bw_indra_01.jpg", "", "", ""],
+		["img/bw_akiem_01.jpg", "", "", ""],
+		["img/bw_jacek_01.jpg", "", "", ""],
+		["img/bw_jasso_01.jpg", "", "", ""],
+		["img/bw_martin_01.jpg", "", "", ""] //<--no trailing comma after very last image element!
 	],
 	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:true},
 	persist: true, //remember last viewed slide and recall within same session?
